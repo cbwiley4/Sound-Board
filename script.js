24d5d0f238ecc3ca40c191a395e4cf2f95fe02bb
caitlyn <cbwiley4@icloud.com>
2026-09-02 12:27:58 -0400

    generate buttons

---
+++ script.js
@@ -0,0 +1,17 @@
+// Create an array of sounds 
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+// For each sound, create a button, add a class 'btn', update the text, append to DOM
+sounds.forEach((sound) => {
+    // Create a button 
+    const btn = document.createElement('button');
+
+    // Add a class of 'btn' to the button 
+    btn.classList.add('btn');
+
+    // Set the button label text to be the same as the string value in the sounds array
+    btn.innerText = sound;
+
+    //add the button to the DOM
+    document.getElementById('buttons').appendChild(btn);
+});
