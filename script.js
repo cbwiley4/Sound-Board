58698e692081785d20f8227340d4c4678a345a81
caitlyn <cbwiley4@icloud.com>
2026-09-02 18:58:04 -0400

    new sounds

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Create an array of sounds 
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const sounds = ['shield', 'default', 'downed', 'storm', 'chest', 'battlebus'];
 
 // For each sound, create a button, add a class 'btn', update the text, append to DOM
 sounds.forEach((sound) => {
@@ -12,6 +12,23 @@ sounds.forEach((sound) => {
     // Set the button label text to be the same as the string value in the sounds array
     btn.innerText = sound;
 
+    //add event listener to this button
+    btn.addEventListener('click', () => {
+        // stop all sounds first
+        stopSounds();
+        document.getElementById(sound).play(); 
+    })
+
     //add the button to the DOM
     document.getElementById('buttons').appendChild(btn);
 });
+
+//helper function
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const snd = document.getElementById(sound);
+
+        snd.pause();
+        snd.currentTime = 0;
+    });
+}
